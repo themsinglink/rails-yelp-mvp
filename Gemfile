b89706ea423bb322ac5ee134efc144444ee655d5
Katy Link <ktelink@gmail.com>
2020-02-19 17:09:34 +0100

    new restaurant app

--- Gemfile
+++ Gemfile
@@ -52,6 +52,12 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+
+gem 'bootstrap-sass', '~> 3.4.1'
+gem 'sassc-rails', '>= 2.1.0'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Destroying Restaurants..."
+Restaurant.destroy_all
+
+puts "Creating Restaurants"
+
+Restaurant.create(
+  name: "Il Giardino",
+  address: "Biarritz",
+  category: "italian",
+)
+
+Restaurant.create(
+  name: "Ramen 4ever",
+  address: "NYC",
+  category: "japanese",
+)
+
+Restaurant.create(
+  name: "Cafe du Commerce",
+  address: "Biarritz",
+  category: "french",
+)
+
+Restaurant.create(
+  name: "Cafe Jean",
+  address: "Biarritz",
+  category: "french",
+)
+
+Restaurant.create(
+  name: "Pepe Pizza",
+  address: "Biarritz",
+  category: "italian",
+)
+
+puts "#{Restaurant.count} restaurants have been created"
+
+
+
